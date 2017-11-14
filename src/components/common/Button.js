a71caedd5ebe4903083fef0b43bacbeb3ef22ef8
batuhansahan <batuhansahan@trakya.edu.tr>
2017-11-14 14:37:57 +0200

    Copied common components from old project

---
+++ src/components/common/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+    const { buttonStyle, buttonText } = styles;
+    return (
+        <TouchableOpacity onPress={onPress} style={buttonStyle}>
+            <Text style={buttonText}>
+                {children}
+            </Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = {
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        height:40,
+        borderWidth: 1,
+        borderColor: '#007aff',
+        marginLeft: 5,
+        marginRight: 5
+    },
+    buttonText: {
+        alignSelf: 'center',
+        fontSize: 16,
+        fontWeight: '600',
+        color: '#007aff',
+        paddingTop: 10,
+        paddingBottom: 10
+    }
+}
+
+export { Button };
