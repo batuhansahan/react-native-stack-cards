3b9f567d3597066937363945ab90d66754e1231c
batuhansahan <batuhansahan@trakya.edu.tr>
2017-11-14 16:21:23 +0200

    LibraryList redux,reducer,json things

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { View,Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
-            <View><Text>HiTest1Test2</Text></View>
+            <View>
+                <Header headerText="Card Stack" />  
+                <LibraryList />
+            </View>
         </Provider>
     );
 };
