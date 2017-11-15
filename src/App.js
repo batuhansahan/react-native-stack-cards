49d2393eae5467d5c8b1af1bcc6a70d51a1aaa57
batuhansahan <batuhansahan@trakya.edu.tr>
2017-11-15 14:41:23 +0200

    managed Selecting List Item

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { View,Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
-import { Header } from './components/common';
+import { Card, Header } from './components/common';
 import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
+            
             <View style={{ flex:1 }}>
                 <Header headerText="Card Stack" />  
                 <LibraryList />
